d5bf1b2b571ba51e5ffda666128f97cc73386c0f
takatori <takatorisatoshi@team-lab.com>
2016-08-09 23:54:58 +0900

    Remove Slick Components

--- test/com/takatori/hundredthings/modules/MockDatabaseModule.scala
+++ test/com/takatori/hundredthings/modules/MockDatabaseModule.scala
@@ -2,8 +2,7 @@ package com.takatori.hundredthings.modules
 
 import _root_.slick.driver.JdbcProfile
 import play.api.db.slick.DbName
-import play.api.db.slick.SlickComponents //
 
-trait MockDatabaseModule extends SlickComponents {
-  lazy val dbConfig = api.dbConfig[JdbcProfile](DbName("test"))
+trait MockDatabaseModule {
+  lazy val dbConfig = dbConfig[JdbcProfile](DbName("test"))
 }
