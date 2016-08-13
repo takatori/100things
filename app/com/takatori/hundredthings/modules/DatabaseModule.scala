86b1262df3bc92ef4b497f8bb5a0de2499e1da09
takatori <takatorisatoshi@team-lab.com>
2016-08-13 17:18:39 +0900

    Add Fetch User

--- app/com/takatori/hundredthings/modules/DatabaseModule.scala
+++ app/com/takatori/hundredthings/modules/DatabaseModule.scala
@@ -1,9 +1,7 @@
 package com.takatori.hundredthings.modules
 
-// _root_ 明示的に絶対指定する
 import _root_.slick.driver.JdbcProfile
-import play.api.db.slick.DbName // The name used in the application's config file to reference a slick database configuration.
-import play.api.db.slick.SlickComponents //
+import play.api.db.slick.{DbName, SlickComponents}
 
 trait DatabaseModule extends SlickComponents {
   lazy val dbConfig = api.dbConfig[JdbcProfile](DbName("default"))
--- test/com/takatori/hundredthings/modules/MockDatabaseModule.scala
+++ test/com/takatori/hundredthings/modules/MockDatabaseModule.scala
@@ -1,8 +1,12 @@
 package com.takatori.hundredthings.modules
 
 import _root_.slick.driver.JdbcProfile
-import play.api.db.slick.DbName
+import play.api.db.slick.{DbName, SlickApi }
+import slick.backend.DatabaseConfig
 
 trait MockDatabaseModule {
-  lazy val dbConfig = dbConfig[JdbcProfile](DbName("test"))
+  def api: SlickApi
+  // SlickComponentsがapiフィールドを持つ
+  // SlickComponentsは抽象フィールド, environment configuration applicationLifeCycleをもつ
+  lazy val dbConfig: DatabaseConfig[JdbcProfile] = api.dbConfig[JdbcProfile](DbName("test"))
 }
