6257684e09edaf4572bd9ffe7510781203f49ba1
takatori <takatorisatoshi@team-lab.com>
2016-08-22 21:46:03 +0900

    Add Create User Dao test

--- test/com/takatori/hundredthings/dao/UserDaoSpec.scala
+++ test/com/takatori/hundredthings/dao/UserDaoSpec.scala
@@ -8,14 +8,14 @@ import org.specs2.mutable.Specification
 class UserDAOSpec(implicit ee: ExecutionEnv) extends Specification {
 
   "UserDAO" should {
-    "return all" in new WithApplicationComponents with DaoContext {}
+    "return all" in new WithApplicationComponents with DaoContext { }
 
     "fetch an user by id" in new WithApplicationComponents with DaoContext {
       userDao.fetch(1) must beEqualTo(Some(User(1, "takatori"))).await
     }
 
     "create user" in new WithApplicationComponents with DaoContext {
-
+      userDao.insert(User(100, "specs2")) must beEqualTo(3).await
     }
   }
 }
